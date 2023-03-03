04c0eb11ac2cf0e98c8a03a8693bf3cab492bb79
KWARN KHAM <kwarnkham.moon@gmail.com>
2023-03-03 11:42:50 +0630

    handle noti click

--- src-pwa/custom-service-worker.js
+++ src-pwa/custom-service-worker.js
@@ -8,3 +8,26 @@ import { precacheAndRoute } from 'workbox-precaching'
 
 // Use with precache injection
 precacheAndRoute(self.__WB_MANIFEST)
+
+self.addEventListener('notificationclick', function (event) {
+  event.notification.close();
+
+  event.waitUntil(
+    clients.matchAll({ type: 'window', includeUncontrolled: true })
+      .then(function (clientList) {
+        if (clientList.length > 0) {
+          let client = clientList[0];
+
+          for (let i = 0; i < clientList.length; i++) {
+            if (clientList[i].focused) {
+              client = clientList[i];
+            }
+          }
+
+          return client.focus();
+        }
+
+        return clients.openWindow('/');
+      })
+  );
+});
